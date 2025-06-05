98710a348b5d77386d660a1848684c1be19be6d0
devilshiv-07 <er.shivank07@gmail.com>
2025-06-05 10:31:40 +0530

    Login and SingUp page is done

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,9 +1,77 @@
-import React from 'react'
+import React, { useState } from "react";
+import { FaUnlockKeyhole } from "react-icons/fa6";
+import { MdOutlineEmail } from "react-icons/md";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const [formData, setFormData] = useState({
+    email: "",
+    password: ""
+  });
 
-export default Login
+  const handlechange = (e) => {
+    setFormData({...formData, [e.target.name]: [e.target.value]})
+  };
+
+  return (
+    <div className="w-full h-[92vh] md:h-[90vh] flex items-center justify-center -mt-16 sm:-mt-6 lg:-mt-8">
+      {/* form div */}
+      <div className="form_container w-76 md:w-86 lg:w-90 flex items-center justify-center relative overflow-hidden z-10">
+        <div className="form w-full m-1 px-8 md:px-10 py-16 flex flex-col items-center bg-black z-20">
+
+          {/* Login Heading */}
+          <h2 className="text-5xl mb-14">Login</h2>
+
+          {/* Form */}
+          <form method="post" className="w-full">
+            {/* name div */}
+            <div className="flex items-center mb-10">
+              <label>
+                <MdOutlineEmail size={27} />
+              </label>
+              <input
+                name="email"
+                value={formData.email}
+                onChange={handlechange}
+                className="w-full px-3 border-b py-1 text-lg placeholder-teal-400"
+                type="email"
+                placeholder="Email"
+              />
+            </div>
+
+            {/* password div */}
+            <div className="flex items-center mb-14">
+              <label>
+                <FaUnlockKeyhole size={21} />
+              </label>
+              <input
+                name="password"
+                value={formData.password}
+                onChange={handlechange}
+                className="w-full px-3 border-b text-lg placeholder-teal-400"
+                type="password"
+                placeholder="Password"
+              />
+            </div>
+
+            {/* Login Button */}
+            <button
+              className="w-full border-1 border-yellow-300 py-1 text-lg tracking-wide rounded-full mb-8"
+              type="submit"
+            >
+              LOGIN
+            </button>
+          </form>
+
+          {/* FP and Register */}
+          <div className="w-full text-teal-400 flex justify-between items-center">
+            <p>Don't have an Account</p>
+            <Link to="/signup">SingUp</Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,9 +1,129 @@
-import React from 'react'
+import { useState } from "react";
+import { FaUnlockKeyhole } from "react-icons/fa6";
+import { FaUser } from "react-icons/fa";
+import { MdOutlineEmail } from "react-icons/md";
+import { TbUserQuestion } from "react-icons/tb";
+import { FaPhoneAlt } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
-const SignUp = () => {
+const Login = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    phone: "",
+    role: "",
+  });
+
+  const handlechange = (e) => {
+    setFormData({...formData, [e.target.name]: [e.target.value]})
+  };
+
   return (
-    <div>SignUp</div>
-  )
-}
+    <div className="w-full h-[92vh] md:h-[90vh] flex items-center justify-center -mt-6 sm:-mt-4">
+      {/* form div */}
+      <div className="form_container w-76 md:w-90 lg:w-100 flex items-center justify-center relative overflow-hidden z-10">
+        <div className="form w-full m-1 px-8 md:px-12 py-10 flex flex-col items-center bg-black z-20">
 
-export default SignUp
+          {/* Login Heading */}
+          <h2 className="text-4xl mb-12 mt-2">SignUp</h2>
+
+          {/* Form */}
+          <form method="post" className="w-full">
+
+            {/* Name div */}
+            <div className="flex items-center mb-8">
+              <label>
+                <FaUser size={18} />
+              </label>
+              <input
+                name="name"
+                value={formData.name}
+                onChange={handlechange}
+                className="w-full px-3 border-b py-1 placeholder-teal-400"
+                type="text"
+                placeholder="Name"
+              />
+            </div>
+
+            {/* Email div */}
+            <div className="flex items-center mb-8">
+              <label>
+                <MdOutlineEmail size={22} />
+              </label>
+              <input
+                name="email"
+                value={formData.email}
+                onChange={handlechange}
+                className="w-full px-2 border-b py-1 placeholder-teal-400"
+                type="email"
+                placeholder="Email"
+              />
+            </div>
+
+            {/* Password div */}
+            <div className="flex items-center mb-8">
+              <label>
+                <FaUnlockKeyhole size={18} />
+              </label>
+              <input
+                name="password"
+                value={formData.password}
+                onChange={handlechange}
+                className="w-full px-3 border-b py-1 placeholder-teal-400"
+                type="password"
+                placeholder="Password"
+              />
+            </div>
+
+            {/* Phone div */}
+            <div className="flex items-center mb-8">
+              <label>
+                <FaPhoneAlt size={18} />
+              </label>
+              <input
+                name="phone"
+                value={formData.phone}
+                onChange={handlechange}
+                className="w-full px-3 border-b py-1 placeholder-teal-400"
+                type="text"
+                placeholder="Phone"
+              />
+            </div>
+
+            {/* Role div */}
+            <div className="flex items-center mb-10">
+              <label>
+                <TbUserQuestion size={18} />
+              </label>
+              <input
+                name="role"
+                value={formData.role}
+                onChange={handlechange}
+                className="w-full px-3 border-b py-1 placeholder-teal-400"
+                type=""
+                placeholder="Role: [Content-creator, User]"
+              />
+            </div>
+
+            {/* Login Button */}
+            <button
+              className="w-full border-1 border-yellow-300 py-1 text-lg tracking-wide rounded-full mb-6"
+              type="submit"
+            >
+              SignUp
+            </button>
+          </form>
+
+          {/* FP and Register */}
+          <div className="w-full text-teal-400 flex justify-between items-center">
+            <p>Already have account</p>
+            <Link to="/login">Login</Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
