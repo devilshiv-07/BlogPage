a7c71bce290b61034273a7668802b874985bad2d
devilshiv-07 <er.shivank07@gmail.com>
2025-06-09 23:03:58 +0530

    axios baseUrl updated

--- frontend/src/http/index.js
+++ frontend/src/http/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_BACKEND_URL,
+    baseURL: "https://blogpage-ln3o.onrender.com/",
     withCredentials: true,
     headers: {
         "Content-Type": "application/json",
